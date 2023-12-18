9bec1ae1399107166acf0cd59e9dcf45e36bfaa6
Ashlingo <otooleashling@gmail.com>
2023-12-18 21:39:42 +0000

    updated display temp function

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ function displayTemperature(response) {
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   iconElement.innerHTML = `<img src = "${response.data.condition.icon_url}" class = "weather-app-icon" />`;
+  getForecast(response.data.city);
 }
 
 function search(event) {
